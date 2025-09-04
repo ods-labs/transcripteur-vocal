76856107c8e90fb5686ef69ebc2ee381cbd350ea
Frédéric Passaniti <f.passaniti@gmail.com>
2025-09-04 14:17:34 +0200

    go

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configuration pour AWS Amplify
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
